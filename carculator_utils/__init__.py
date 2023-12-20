c2462c4e0a973322a80ea79c33d9109780a4aeb6
Hugo Chesneau <chesneau.hugo@gmail.com>
2023-12-20 10:42:33 +0100

    moved klausen within this package for improved maintainability

--- carculator_utils/__init__.py
+++ carculator_utils/__init__.py
@@ -16,6 +16,7 @@ from .driving_cycles import get_standard_driving_cycle_and_gradient
 from .export import ExportInventory
 from .hot_emissions import HotEmissionsModel
 from .inventory import Inventory
+from .named_parameters import NamedParameters
 from .noise_emissions import NoiseEmissionsModel
 from .vehicle_input_parameters import VehicleInputParameters
 
@@ -27,4 +28,5 @@ __all__ = (
     "BackgroundSystemModel",
     "ExportInventory",
     "VehicleInputParameters",
+    "NamedParameters",
 )
